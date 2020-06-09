0ebf50c63e0d261d4c706548a53be18ca43a41cf
Antonio Pugliese <a.pugliese@zakeke.com>
2020-06-09 11:03:32 +0200

    'Dist'

--- publish.bat
+++ publish.bat
@@ -4,4 +4,4 @@ call git add .
 call git commit -m 'Dist'
 call git push
 cd C:\Users\Antonio\Documents\Lavoro\Zakeke_Project\Zakeke
-call npm install AntonioPugliese74/react-form-generator --save
+call npm install UpCommerce/react-form-generator --save
